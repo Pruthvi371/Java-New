b857650b0bc8b9d5e1b2b9f749d9fb43a916849b
Pruthvi371 <pruthvi371@gmail.com>
2020-02-07 18:30:43 +0530

    first commit

---
+++ src/MainMethodClass.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+
+
+public class MainMethodClass {
+
+	public static void main(String[] args) {
+		
+
+		/*
+		 Below StudentUtil Class output has been printed 
+		 */
+		
+		int[] studentIdList = {1001,1002};
+		
+		char[][] studentsGrades = {{'A','B'},{'A'}};
+		
+		double[] d =StudentUtil.calculateGPA(studentIdList, studentsGrades);
+		System.out.println(d[0]);
+		
+		int [] i = StudentUtil.getStudentsByGPA(3.2, 3.5, studentIdList, studentsGrades);
+		System.out.println(i[0]);
+		
+		
+		
+		/*
+		 Below code is for printing the output of SentimentAnalyzer Class
+		 */
+		
+		
+//		String review = "Haven't been here in years! Fantastic service and the food was delicious! Definetly will be a frequent flyer! Francisco was very attentive".toLowerCase();
+		
+		String review = "Sorry OG, but you just lost some loyal customers. Horrible service, no smile or greeting just attitude. The breadsticks were stale and burnt, appetizer was cold and the food came out before the salad.".toLowerCase();
+		
+		String[][] featureSet = { 
+		        { "ambiance", "ambience", "atmosphere", "decor" },
+				{ "dessert", "ice cream", "desert" }, 
+				{ "food" }, 
+				{ "soup" },
+				{ "service", "management", "waiter", "waitress", "bartender", "staff", "server" } };
+		String[] posOpinionWords = { "good", "fantastic", "friendly", "great", "excellent", "amazing", "awesome",
+				"delicious" };
+		String[] negOpinionWords = { "slow", "bad", "horrible", "awful", "unprofessional", "poor" };
+
+		int[] featureOpinions = SentimentAnalyzer.detectProsAndCons(review, featureSet, posOpinionWords, negOpinionWords);
+		System.out.println("Opinions on Features: " + Arrays.toString(featureOpinions));
+
+	}
+
+}
---
+++ src/SentimentAnalyzer.java
@@ -0,0 +1,75 @@
+
+
+public class SentimentAnalyzer {
+	
+	public static int[] detectProsAndCons(String review, String[][] featureSet, String[] posOpinionWords,
+			String[] negOpinionWords) {
+		
+		int[] featureOpinions = new int[featureSet.length]; // output
+		
+		for(int i = 0 ; i<featureSet.length ;i++) {
+			for(String s :featureSet[i]) {
+				if(review.contains(s)) 
+			featureOpinions[i] = getOpinionOnFeature(review, s, posOpinionWords, negOpinionWords);
+			}
+		}
+ 
+		return featureOpinions;
+	}
+
+	private static int getOpinionOnFeature(String review, String feature, String[] posOpinionWords, String[] negOpinionWords) {
+		
+		int opinion = 0;
+		opinion = checkForWasPhrasePattern(review, feature, posOpinionWords, negOpinionWords);
+		opinion= checkForOpinionFirstPattern(review, feature, posOpinionWords, negOpinionWords);
+		return opinion;
+	}	
+
+	private static int checkForWasPhrasePattern(String review, String feature, String[] posOpinionWords, String[] negOpinionWords) {
+		
+		int opinion = 0;
+		String pattern = feature + " was ";
+
+		for(int j =0 ; j<posOpinionWords.length;j++) {
+		
+			String opString =  posOpinionWords[j];
+			if(review.contains(opString)&&
+			  (review.indexOf(opString)==review.indexOf(pattern)+pattern.length()+1))  
+			 opinion = 1;
+			
+		}
+		
+		for(int i = 0 ; i<negOpinionWords.length;i++) {
+			
+			String negString = negOpinionWords[i];
+			if (review.contains(negString)&&
+			   (review.indexOf(negString)==review.indexOf(pattern)+pattern.length()+1)) 
+				opinion = -1;
+		}
+		return opinion; 	
+	}
+	
+	private static int checkForOpinionFirstPattern(String review, String feature
+		,String[] posOpinionWords,String[] negOpinionWords) {
+		
+		int opinion = 0;
+		for(int j =0 ; j<posOpinionWords.length;j++) {
+			String opString =  posOpinionWords[j];
+			if(review.contains(opString)&&
+			  (review.indexOf(feature)==review.indexOf(opString)+opString.length()+1))  
+			  opinion = 1;
+		
+		}
+		
+		
+		for(int i = 0 ; i<negOpinionWords.length;i++) {
+			String negString = negOpinionWords[i];
+			if (review.contains(negString)&&
+			   (review.indexOf(feature)==review.indexOf(negString)+negString.length()+1)) 
+				opinion = -1;
+		}
+		
+		return opinion;
+	}
+	
+}
